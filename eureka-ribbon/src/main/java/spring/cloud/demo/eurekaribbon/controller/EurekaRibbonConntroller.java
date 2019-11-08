012df3c061ef07b5a86b5c26df204f4ac7a9671a
fujie feng <fujie.feng@qunar.com>
2019-11-08 10:45:52 +0800

    增加Sleuth分布式链路追踪 依赖

--- eureka-ribbon/src/main/java/spring/cloud/demo/eurekaribbon/controller/EurekaRibbonConntroller.java
+++ eureka-ribbon/src/main/java/spring/cloud/demo/eurekaribbon/controller/EurekaRibbonConntroller.java
@@ -1,10 +1,13 @@
 package spring.cloud.demo.eurekaribbon.controller;
 
+import brave.Tracer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.cloud.demo.eurekaribbon.service.EurekaRibbonService;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @auther: fujie.feng
@@ -14,14 +17,21 @@ import spring.cloud.demo.eurekaribbon.service.EurekaRibbonService;
 @Slf4j
 public class EurekaRibbonConntroller {
 
+//    Logger log = LoggerFactory.getLogger(EurekaRibbonConntroller.class);
+
     @Autowired
     private EurekaRibbonService eurekaRibbonService;
 
+    @Autowired
+    private Tracer tracer;
+
     @RequestMapping("/sayHello")
-    public String syaHello() {
+    public String syaHello(HttpServletRequest request) {
+
         log.info("eureka-ribbon server......");
         String message = eurekaRibbonService.sayHello();
-        log.info("[eureka-ribbon][EurekaRibbonConntroller][syaHello], message={}", message);
+        log.info(">>>traceId={}, spanId={}", request.getHeader("X-B3-TraceId"), request.getHeader("X-B3-SpanId"));
+        log.info("[eureka-ribbon][EurekaRibbonConntroller][syaHello], message={}, traceId={}", message, tracer.currentSpan().context().traceIdString());
         return "ribbon result: " + message;
     }
 }
